bc474036391bc0f78aeda0f7635e588ac7196615
Thorvald Aagaard <thorvald@aalborgdata.dk>
2025-03-13 23:46:30 +0100

    Version 0.8..6.3 Fixed vulnerability error for BBA-simulation UI changes for review of board Added reward for play card recommended of NN Added API for claim Update of sampling

--- src/bba/test_bid hand.py
+++ src/bba/test_bid hand.py
@@ -8,12 +8,12 @@ our_system_file = "../../BBA/CC/GIB-BBO.bbsa"
 their_system_file = "../../BBA/CC/GIB-BBO.bbsa"
 position = 2
 hand = "AT762.K74.Q8.654"
-vuln = [True, True]
+vuln = [False, True]
 dealer = 0
 scoring_matchpoint = False
 verbose = False
 
 bot = BBABotBid(our_system_file, their_system_file, position, hand, vuln, dealer, scoring_matchpoint, verbose)
-bid = bot.bid(['PASS', '1H'])
+bid = bot.bid_hand(["1H"], "N:5.AQ8652.AK.AQT7 AT762.K74.Q8.654 QJ43.T9.J9732.92 K98.J3.T654.KJ83")
 print(bid)
 
